a3340d91d0d0a67f5ee68d6ee2c4f49f2de2b8e4
Spalter <92591301+varunsingh04@users.noreply.github.com>
2025-02-15 22:38:54 +0530

    created user aut checked with postman but geeting an error

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,22 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const connectToDb = require('./db/db.js')
+const userRoutes = require('./routes/user.routes.js');
+
+
+connectToDb();
 
 const app = express()
-
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-app.get('/test',function(req,res){
+app.get('/',function(req,res){
     res.send("Testing te setup")
 })
+
+app.use('/user',userRoutes);
 
 
 module .exports = app;
